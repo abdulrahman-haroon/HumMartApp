a19f29e7b744a16e902371faffadaa7bbef1b3f6
abdulrahman-haroon <abdulrahman.haroonr@gmail.com>
2020-08-29 19:16:25 +0500

    Added NetInfo for checking if there is wifi or not

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
 import FlashMessage from "react-native-flash-message";
 import DrawerNavigations from "./app/Navigations/DrawerNavigations";
 import WelcomeLoad from "./app/Screens/welcomeLoading";
@@ -6,8 +7,11 @@ import WelcomeLoad from "./app/Screens/welcomeLoading";
 import { Provider } from "react-redux";
 import store from "./app/src/store";
 
+import { useNetInfo } from "@react-native-community/netinfo";
+
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const netinfo = useNetInfo();
 
   useEffect(() => {
     setTimeout(() => {
@@ -17,7 +21,13 @@ export default function App() {
   if (isLoading) {
     return <WelcomeLoad />;
   }
-
+  if (netinfo.type !== "unknown" && netinfo.isInternetReachable === false) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Sorry! can't seem to connect. Check your internet</Text>
+      </View>
+    );
+  }
   return (
     <>
       <Provider store={store}>
